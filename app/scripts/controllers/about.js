360d9b87cc024161829bf91b90ca83c5ef693d32
radames <radames@infoway-pi.com.br>
2016-12-09 10:27:32 -0300

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name crossoverChallengeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the crossoverChallengeApp
+ */
+angular.module('crossoverChallengeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
