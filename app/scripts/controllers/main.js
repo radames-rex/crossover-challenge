181d70fe47e6e3a8552ec82d3b048173c3b5b975
radames-rex <radames.rex@gmail.com>
2016-12-09 10:27:32 -0300

    Initial Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name crossoverChallengeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the crossoverChallengeApp
+ */
+angular.module('crossoverChallengeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
